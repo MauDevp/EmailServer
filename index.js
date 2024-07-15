df4204cabcd315128c2ebf5ea32529dbd192f26f
Mauricio Silva <mauricioramonsilvasandoval@gmail.com>
2024-07-14 20:49:26 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// Configuración de Nodemailer para Zoho Mail
+const transporter = nodemailer.createTransport({
+    service: 'Zoho',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+// Ruta para manejar el envío de formularios
+app.post('/send-email', (req, res) => {
+    const { toEmail, subject, message } = req.body;
+
+    const mailOptions = {
+        from: 'mauricio@maudevp.tech',
+        to: toEmail,
+        subject: subject,
+        text: message
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error al enviar el correo:', error);
+            return res.status(500).send('Error al enviar el correo.');
+        }
+        console.log('Correo enviado:', info.response);
+        res.status(200).send('Correo enviado correctamente.');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
